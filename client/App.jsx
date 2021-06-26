3ccf9887697ada4637245e6e2cd1f30b9e131ed8
Sam Arnold <samarnold@Sams-MacBook-Pro.local>
2021-06-26 17:35:08 -0400

    Started a toilet card component

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,12 @@
 import React,{useState} from 'react'
 import LoginForm from './Components/loginForm.jsx'
+import ToiletCard from './Components/toiletCard.jsx'
 
 const App = () => (
     <div>
         <h1>Hello React</h1>
         <LoginForm />
+        <ToiletCard />
     </div>
 )
 
